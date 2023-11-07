0609fde22ad52669b35930b0187c33a8a307c41a
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-07 17:09:16 -0500

    fixed exercise 2

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -1,18 +1,26 @@
 from random import randrange
 
-def user_input():
-    user_guess = input("Please enter a number between 0 -and 10  ")
-    return int(user_guess)
+# def user_input():
+#     user_guess = input("Please enter a number between 0 and 10  ")
+#     return int(user_guess)
 
-def random_number():
-    return randrange(11)
+# def random_number():
+#     return randrange(11)
 
-def user_right_or_wrong(num1, num2):
-    if num1 == num2:
-        print("Yes! You guessed the right number.")
-    else:
-        print("No! You guessed the wrong number.")
+# def user_right_or_wrong(num1, num2):
+#     if num1 > num2:
+#         print("No, too high!")
+#     elif num1 < num2:
+#         print("No! Too low!")
+#     else:
+#         print("Yes! You guessed correctly!")
+# guess = user_input()
+# random = random_number()
 
-print(f"Your number: {user_input()}")
-print(f"Random number: {random_number()}")
-user_right_or_wrong(user_input, random_number)
+
+# user_right_or_wrong(guess, random)
+
+# print(f"Your number: {guess}")
+# print(f"Random number: {random}")
+
+print(randrange(10))
