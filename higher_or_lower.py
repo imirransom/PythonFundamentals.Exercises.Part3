217958a766e901bfee037242e4489bc5b1473eaf
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-07 21:00:05 -0500

    did higher or lower continued

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -1,26 +1,25 @@
 from random import randrange
 
-# def user_input():
-#     user_guess = input("Please enter a number between 0 and 10  ")
-#     return int(user_guess)
+def user_input():
+    user_guess = input("Please enter a number between 0 and 10  ")
+    return int(user_guess)
 
-# def random_number():
-#     return randrange(11)
+def random_number():
+    return randrange(11)
 
-# def user_right_or_wrong(num1, num2):
-#     if num1 > num2:
-#         print("No, too high!")
-#     elif num1 < num2:
-#         print("No! Too low!")
-#     else:
-#         print("Yes! You guessed correctly!")
-# guess = user_input()
-# random = random_number()
+def user_right_or_wrong(num2):
+    while True:
+        guess = user_input()
+        if guess > num2:
+            print("No, too high!")
+        elif guess < num2:
+            print("No! Too low!")
+        else:
+            print("Yes! You guessed correctly!")
+        
+random = random_number()
 
-
-# user_right_or_wrong(guess, random)
+user_right_or_wrong(random)
 
 # print(f"Your number: {guess}")
 # print(f"Random number: {random}")
-
-print(randrange(10))
